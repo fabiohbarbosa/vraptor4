da955aa0fb94f8e676d011886f41ad72c09b198f
Fábio H. G. Barbosa <fabio.barbosa@gauge.com.br>
2014-09-07 20:43:56 -0300

    Exemplo de um projeto com VRaptor 4 http://fabiohbarbosa.wordpress.com

---
+++ src/main/java/com/wordpress/fabiohbarbosa/model/entity/Empresa.java
@@ -0,0 +1,37 @@
+package com.wordpress.fabiohbarbosa.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Empresa {
+	@Id 
+	@SequenceGenerator(name="empresa_seq",sequenceName="empresa_seq", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="empresa_seq")
+	private Long id;
+	private String nome;
+	private Long documento;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Long getDocumento() {
+		return documento;
+	}
+	public void setDocumento(Long documento) {
+		this.documento = documento;
+	}
+	
+}
